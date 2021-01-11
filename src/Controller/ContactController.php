fa051653a2e77f5fbbb79993f99723ff4c3cf619
Christel-WF3 <christelmirassou2@gmail.com>
2021-01-11 12:29:57 +0100

    ajout commentaire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -30,6 +30,7 @@ class ContactController extends AbstractController
      */
     public function new(Request $request): Response
     {
+        //FORMULAIRE DE CREATION DE CONTACT
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
